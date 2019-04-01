19b89aae5f79eeec271a2e62f0765c56fbe907fc
Олександр Затогін <zatogin1995@gmail.com>
2019-04-01 14:01:24 +0300

    Add project files.

---
+++ pre-final/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pre_final.Startup))]
+namespace pre_final
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
